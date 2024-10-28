9daa273ae1d18047c6343b08a9fde00e625bd3f3
nikolozieliozishvili <nikolozeliozi@gmail.com>
2024-10-28 10:46:44 -0700

    Add files via upload

---
+++ GOA01/Level 21/classwork/1.py
@@ -0,0 +1,13 @@
+("bmw", mersedes ,audi)
+print = (insert2)
+
+
+#imsert os kontrorilebadia 
+
+#append ukontroloa
+
+
+zip   v. dfdfkvdbwon wrobwt0bheo ngh[moik]mpoyu]6 kontrorilebadia#2
+
+
+ movies = ["Avatar", "Titanic", "Avengers", "GOA" , "Group 30"] 
